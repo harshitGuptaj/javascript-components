fd994b7a350e2c4e6133f58f9f8910b57be8e89a
harshitGupta <hargupta2004@gmail.com>
2025-11-03 06:26:46 +0000

    commit changes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as new version
+ 
+//alert(3+3)// we are using nodejs , not browser
+
+console.log( 5
+    +
+    5
+);//code readibility should be high
+
+console.log("harshit");
+
+let name = "harshit"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined => 
+//symbol=> unique
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
